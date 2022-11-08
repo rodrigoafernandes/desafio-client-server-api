03bbffbde2dab1201232633ac15d11cfe558d0f9
Rodrigo Fernandes <rodrigoforit@gmail.com>
2022-11-08 17:47:22 -0300

    Configuracao da instancia do sqlite para a aplicacao

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/rodrigoafernandes/desafio-client-server-api/config"
+	"github.com/rodrigoafernandes/desafio-client-server-api/cotacao"
+	"github.com/rodrigoafernandes/desafio-client-server-api/db"
+)
 
+func main() {
+	config.SetupServer()
+	db.SetupDatabase(config.ServerCFG)
+
+	var ct cotacao.CotacaoDB
+
+	db.DB.QueryRow(`SELECT
+    						id, code, codein, name, high, low, varBid, ask, timestamp, create_date
+					FROM cotacoes`).Scan(
+		&ct.ID,
+		&ct.Code,
+		&ct.CodeIn,
+		&ct.Name,
+		&ct.High,
+		&ct.Low,
+		&ct.VarBid,
+		&ct.PctChange,
+		&ct.Bid,
+		&ct.Ask,
+		&ct.Timestamp,
+		&ct.CreateDate)
+	fmt.Println(ct)
+	fmt.Println("server app")
 }
