0e85e3298bb6b89c8397ee1f0856d3e789f1622f
Rodrigo Fernandes <rodrigoafernandes@users.noreply.github.com>
2022-11-10 10:25:27 -0300

    Adicionado client para consumo da cotacao gerada pela app Server (#7)

--- client/client.go
+++ client/client.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"github.com/rodrigoafernandes/desafio-client-server-api/config"
 	"github.com/rodrigoafernandes/desafio-client-server-api/output"
+	"github.com/rodrigoafernandes/desafio-client-server-api/ws"
 )
 
 func main() {
 	config.SetupClient()
+	cotacaoClient, err := ws.NewCotacaoWSClient(config.ClientCFG)
+	if err != nil {
+		panic(err)
+	}
 	ro, err := output.NewResultOutput(config.ClientCFG)
 	if err != nil {
 		panic(err)
 	}
-	err = ro.WriteQuotationResult(5.1592)
+	bid, err := cotacaoClient.GetUSDBRLQuotation()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("client app")
+	err = ro.WriteQuotationResult(bid)
+	if err != nil {
+		panic(err)
+	}
 }
